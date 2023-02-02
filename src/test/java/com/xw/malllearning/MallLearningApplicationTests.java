4efbd97540741eb237e4531ba078f7aa965a6064
xw <1811498080@qq.com>
2023-02-02 15:41:13 +0800

    Initial commit

---
+++ src/test/java/com/xw/malllearning/MallLearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.xw.malllearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MallLearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
