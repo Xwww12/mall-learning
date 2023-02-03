96faa78d912fc6c55a27e950b74c8b1c9466a60e
xw <1811498080@qq.com>
2023-02-03 19:03:35 +0800

    项目基本框架

---
+++ src/main/java/com/xw/mallLearning/generator/mbg/mapper/PmsBrandMapper.java
@@ -0,0 +1,18 @@
+package com.xw.mallLearning.generator.mbg.mapper;
+
+import com.xw.mallLearning.generator.mbg.entity.PmsBrand;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * <p>
+ * 品牌表 Mapper 接口
+ * </p>
+ *
+ * @author xw
+ * @since 2023-02-03
+ */
+@Mapper
+public interface PmsBrandMapper extends BaseMapper<PmsBrand> {
+
+}
